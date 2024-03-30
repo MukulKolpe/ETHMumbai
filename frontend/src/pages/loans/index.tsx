bf608edb6a6345e06d22e157127ae398db555e8a
Mukul Kolpe <mukulkolpe45@gmail.com>
2024-03-30 14:06:30 +0530

    Mapped all the loans on the loans page

--- frontend/src/pages/loans/index.tsx
+++ frontend/src/pages/loans/index.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+// @ts-nocheck comment
+import React, { useState, useEffect } from "react";
+import CardComponent from "../../components/CardComponent/CardComponent";
+import {
+  Grid,
+  GridItem,
+  Center,
+  Button,
+  Flex,
+  Stack,
+  useColorModeValue,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Container,
+  SimpleGrid,
+  Icon,
+} from "@chakra-ui/react";
+import { ethers } from "ethers";
+import LoanManager from "../../utils/LoanManager.json";
 
-const Loans = () => {
-  return <div>Loans</div>;
-};
+export default function Loans() {
+  const [loans, setLoans] = useState([]);
+  useEffect(() => {
+    const getLoans = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        "0xA6Fa7cE717b478FcC2A7A68d48DD8509A8095AE1",
+        LoanManager,
+        signer
+      );
 
-export default Loans;
+      const getLength = await contract.getAllLoansLength();
+      const length = getLength.toString();
+      console.log(length);
+      for (let i = 0; i < length; i++) {
+        const loan = await contract.allLoans(i);
+
+        setLoans((loans) => [...loans, loan]);
+      }
+    };
+    getLoans();
+  }, []);
+
+  return (
+    <Grid
+      templateRows="repeat(2, 1fr)"
+      templateColumns="repeat(3, 1fr)"
+      gap={4}
+      mr={6}
+    >
+      {loans.map((address) => {
+        return (
+          <GridItem rowSpan={1} colSpan={1}>
+            <CardComponent address={address} />
+          </GridItem>
+        );
+      })}
+    </Grid>
+  );
+}
